bda3f10e033457275d720db51a28281ca57ed2f7
Alejandro_Wong <121511772+Alejandro-Wong@users.noreply.github.com>
2025-06-21 13:04:54 -0400

    login at init, get_latest_post() only returns one dataframe

--- x_posts.py
+++ x_posts.py
@@ -14,11 +14,11 @@ import x_posts
 import getpass
 import importlib
 
-class ScrapeX:
+class XPosts:
     """
         Scrape X (F.K.A Twitter) posts from specified profile(s).
     """
-    def __init__(self, profiles : str | list, headless : bool = False):
+    def __init__(self, profiles : str | list = None, headless : bool = False):
         # Check for .env
         self.__check_for_env()
 
@@ -41,6 +41,8 @@ class ScrapeX:
         # Driver
         self.driver = webdriver.Chrome(options=self.options)
 
+        # Login
+        self.__login()
 
     def __check_for_env(self) -> None:
         """
@@ -48,7 +50,7 @@ class ScrapeX:
         """
         # If .env not in current directory
         if not os.path.exists('./.env'):
-            username = input("Enter username: ")
+            username = input("Enter X username: ")
             password = getpass.getpass("Enter password: ")
 
             # .env file content
@@ -78,71 +80,63 @@ class ScrapeX:
 
         # Username
         username_input = self.driver.find_element(By.XPATH, '//input')
-        username_input.send_keys(self.username)
+        username_input.send_keys(self.username) if self.username else None
         time.sleep(2)
         username_input.send_keys(Keys.ENTER) # Submit
         time.sleep(2)
 
         # Password
         password_input = self.driver.find_element(By.XPATH, '//input[@name="password"]')
-        password_input.send_keys(self.password)
+        password_input.send_keys(self.password) if self.password else None
         time.sleep(2)
         password_input.send_keys(Keys.ENTER) # Submit
         time.sleep(2)
 
-    def get_latest_posts(self) -> dict[str, pd.DataFrame]:
+    def get_latest_posts(self, profile : str) -> pd.DataFrame:
         """
-            Saves the latest ~20 posts from specified profile(s) to pd.DataFrame(s)
+            Saves the latest ~20 posts from specified profiles(s) to pd.DataFrame(s)
             DataFrame(s) include(s):
                 - Datetime
                 - Posts
         """
-        # Login
-        self.__login()
+        # Get desired profiles page
+        self.driver.get(f'https://x.com/search?q={profile}%20-filter%3Amedia%20-filter%3Areplies%20-filter%3Aretweets%20-filter%3Aquote%20-filter%3Alinks&src=typed_query&f=live')
+        time.sleep(3)
 
-        # Empty dict to store pd.DataFrames
-        dfs = {}
+        # User not found
+        not_found = self.driver.find_elements(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div/div[1]/span')
+        if not_found:
+            print(f"{profile} not found, check spelling")
+            return
+        
+        # Empty lists for datetime and posts
+        datetime_idx = []
+        posts = []
 
-        # Iterate through profiles
-        for profile in self.profiles:
+        # Find posts
+        latest_posts = self.driver.find_elements(By.XPATH, '//article[@data-testid="tweet"]')
 
-            # Get desired profile page
-            self.driver.get(f'https://x.com/search?q={profile}%20-filter%3Amedia%20-filter%3Areplies%20-filter%3Aretweets%20-filter%3Aquote%20-filter%3Alinks&src=typed_query&f=live')
-            time.sleep(5)
+        # Separate a clean datetime and post info, append to appropriate lists
+        for post in latest_posts:
+            time_tag = post.find_element(By.XPATH, './/time')
+            datetime_attr = pd.to_datetime(time_tag.get_attribute('datetime'))
+            datetime = datetime_attr.tz_convert(pytz.timezone('America/New_York'))
+            datetime = datetime.strftime('%Y-%m-%d %H:%M')
+            datetime = pd.to_datetime(datetime)
+            datetime_idx.append(datetime)
 
-            # Empty lists for datetime and posts
-            datetime_idx = []
-            posts = []
+            filter_text = post.text.splitlines()
+            text = filter_text[4]
+            posts.append(text)
 
-            # Find posts
-            latest_posts = self.driver.find_elements(By.XPATH, '//article[@data-testid="tweet"]')
+        # Create dataframe of latest posts
+        datetime_series = pd.Series(datetime_idx)
+        posts_series = pd.Series(posts)
 
-            # Separate a clean datetime and post info, append to appropriate lists
-            for post in latest_posts:
-                time_tag = post.find_element(By.XPATH, './/time')
-                datetime_attr = pd.to_datetime(time_tag.get_attribute('datetime'))
-                datetime = datetime_attr.tz_convert(pytz.timezone('America/New_York'))
-                datetime = datetime.strftime('%Y-%m-%d %H:%M')
-                datetime = pd.to_datetime(datetime)
-                datetime_idx.append(datetime)
+        df_dict = {'Datetime':datetime_series, 'Posts':posts_series}
+        df = pd.DataFrame(df_dict)
 
-                filter_text = post.text.splitlines()
-                text = filter_text[4]
-                posts.append(text)
-
-            # Create dataframe of latest posts
-            datetime_series = pd.Series(datetime_idx)
-            posts_series = pd.Series(posts)
-
-            df_dict = {'Datetime':datetime_series, 'Posts':posts_series}
-            df = pd.DataFrame(df_dict)
-
-            # Add DataFrame to dictionary
-            dfs[profile] = df
-
-        self.driver.close()
-
-        return dfs
+        return df
     
     def get_media(self, path : str) -> None:
         """
@@ -171,10 +165,10 @@ class ScrapeX:
         action = ActionChains(self.driver)
 
         # Iterate through profiles
-        for profile in self.profiles:
+        for profiles in self.profiles:
 
             # Get Profile/Media
-            self.driver.get(f'https://x.com/{profile}/media')
+            self.driver.get(f'https://x.com/{profiles}/media')
             time.sleep(3)
 
             # Media section thumbnails
@@ -208,7 +202,7 @@ class ScrapeX:
                 pic = self.driver.find_element(By.XPATH, "/html/body/img")
                 pic.click()
                 time.sleep(1)
-                self.driver.get_full_page_screenshot_as_file(f'{path}/{profile}_{i}.png')
+                self.driver.get_full_page_screenshot_as_file(f'{path}/{profiles}_{i}.png')
                 time.sleep(1)
 
         self.driver.close()
